504e560a37ec3a6b9675e1ecbfafeb8500356cfb
nasra <naasironasra@gmail.com>
2025-06-21 15:31:37 +0300

    update routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import ToolList from './pages/ToolList';
+import ToolDetail from './pages/ToolDetail';
+import Dashboard from './pages/Dashboard';
 import NavBar from './components/NavBar';
 
 function App() {
@@ -10,8 +13,11 @@ function App() {
             <NavBar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/tools" element={<ToolList />} />
+                <Route path="/tools/:id" element={<ToolDetail />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </div>
     );
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
 
 function NavBar() {
-  return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/tools">Browse Tools</Link>
-      <Link to="/login">Login</Link>
-    </nav>
-  );
+    const user = JSON.parse(localStorage.getItem('user'));
+    
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/tools">Browse Tools</Link>
+            {user ? (
+                <Link to="/dashboard">Dashboard</Link>
+            ) : (
+                <Link to="/login">Login</Link>
+            )}
+        </nav>
+    );
 }
 
 export default NavBar;
